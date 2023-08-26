91b368ae09a26c1bba21d6b301c22dedf9518b8d
Jacob Winfrey <jacob_winfrey@yahoo.com>
2023-08-26 15:47:08 -0600

    .

--- keepr_redux/Repositories/Keeps Repository.cs
+++ keepr_redux/Repositories/Keeps Repository.cs
@@ -32,4 +32,67 @@ public class KeepsRepository
     }, keepData).FirstOrDefault();
     return keep;
   }
+
+  internal List<Keep> GetAllKeeps()
+  {
+    string sql = @"
+    SELECT
+    k.*,
+    creator.*
+    FROM keeps k
+    JOIN accounts creator ON k.creatorId = creator.id
+    ;";
+    List<Keep> keeps = _db.Query<Keep, Account, Keep>(sql, (keep, creator) =>
+    {
+    return keep;
+    }).ToList();
+    return keeps;
+  }
+
+  internal Keep GetById(int keepId)
+  {
+    string sql = @"
+    SELECT
+    k.*,
+    creator.*
+    FROM keeps k 
+    JOIN accounts creator ON k.creator.id
+    WHERE k.id = @keepId
+    ;";
+
+    Keep keep = _db.Query<Keep, Account, Keep>(sql, (keep, creator) =>
+    {
+      keep.Creator = creator;
+      return keep;
+    }, new { keepId}).FirstOrDefault();
+    return keep;
+  }
+
+  internal void EditKeep(Keep original)
+  {
+    string sql = @"
+    UPDATE keeps
+    SET
+    name = @name,
+    creatorId = @creatorId,
+    description = @description,
+    img = @img,
+    views = @views,
+    kept = @Kept
+    WHERE id = @id
+    ;";
+
+    _db.Execute(sql, original);
+  }
+
+  internal int DeleteKeep(int keepId)
+  {
+    string sql = @"
+    DELETE FROM keeps
+    WHERE id = @keepId LIMIT 1
+    ;";
+
+    int rows = _db.Execute(sql, new {keepId});
+    return rows;
+  }
 }
